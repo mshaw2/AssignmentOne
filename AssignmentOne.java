944f841e16023f246dad170322eb5ae3f7556081
Manmohan Shaw <shawmanmohan@gmail.com>
2016-06-23 19:47:20 +0530

    Session2:AssignmentOne

--- AssignmentOne.java
+++ AssignmentOne.java
@@ -1,16 +1,29 @@
-package com.sessionone;
+package com.sessiontwo;
 
-/* Write a program to print the sum of two numbers. 
- * One number should be int and another should be float.
- * 
+import java.util.Scanner;
+
+/*
+ * Write a Java program to allow the user to input his/her age.
+ *  Then the program has to show
+ *  whether the person is eligible to vote or not.
  */
+
 public class AssignmentOne {
-	public static void main(String[] args) {
-		int firstnumber=10;
-		float secondnumber=10.67f;
-		float sum = firstnumber + secondnumber;
-		System.out.println("The sum without Typecasting is "+ sum);
-		sum = (int)sum; //Type casting float to integer
-		System.out.println("The sum after Typecasting is "+ sum);
+
+	public static void main(String args[]){
+		
+				System.out.println("Enter Your Age");
+				Scanner sc = new Scanner(System.in);
+				int userAge = sc.nextInt();
+				sc.close();
+				if (userAge >= 18)
+				{
+					System.out.println("You are eligible to vote");
+				}
+				else
+				{
+					System.out.println("You are not eligible to vote");
+				}	
 	}
+
 }
