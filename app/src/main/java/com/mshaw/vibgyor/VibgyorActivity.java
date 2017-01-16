7fd47207d79f9bd05984ccab48340affd0d8e710
mshaw2 <shawmanmohan@gmail.com>
2017-01-16 23:25:38 +0530

    Added Session2 assignment one

---
+++ app/src/main/java/com/mshaw/vibgyor/VibgyorActivity.java
@@ -0,0 +1,13 @@
+package com.mshaw.vibgyor;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class VibgyorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vibgyor);
+    }
+}
